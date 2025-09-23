e130661cc395728da50d6c3212ed710321439143
Minh8x8 <triminh701@gmail.com>
2025-09-23 17:45:08 +0700

    SCRUM-100 FE: Unit test add a new post

---
+++ src/features/post/__tests__/AddPostCard.test.tsx
@@ -0,0 +1,45 @@
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import AddPostCard from "../components/AddPostCard";
+
+jest.mock("../components/AddPostModal", () => {
+  return jest.fn(({ isOpen, onOpenChange }) => {
+    if (isOpen) {
+      return (
+        <div data-testid="mock-modal">
+          <p>Modal is open</p>
+          <button onClick={() => onOpenChange(false)}>Close Modal</button>
+        </div>
+      );
+    }
+    return null;
+  });
+});
+
+describe("🧪 Add Post Card component tests", () => {
+  test("card is rendered", () => {
+    render(<AddPostCard />);
+    const cardElementContent = screen.getByPlaceholderText("What's new");
+    expect(cardElementContent).toBeInTheDocument();
+  });
+
+  test("displays a modal when the input is clicked", async () => {
+    render(<AddPostCard />);
+    const inputElement = screen.getByPlaceholderText("What's new");
+
+    await act(async () => {
+      fireEvent.click(inputElement);
+    });
+
+    expect(screen.getByTestId("mock-modal")).toBeInTheDocument();
+  });
+
+  test("post button opens the modal", async () => {
+    render(<AddPostCard />);
+    const postButton = screen.getByRole("button", { name: /post/i });
+    await act(async () => {
+      fireEvent.click(postButton);
+    });
+
+    expect(screen.getByTestId("mock-modal")).toBeInTheDocument();
+  });
+});
