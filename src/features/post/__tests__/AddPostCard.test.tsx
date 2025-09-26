67605bf856a8d613980727f9a676330383e0a2d1
Phong Nguyen Hoang <phong.nguyenhoang@nashtechglobal.com>
2025-09-26 16:47:38 +0700

    Fix deploy

--- src/features/post/__tests__/AddPostCard.test.tsx
+++ src/features/post/__tests__/AddPostCard.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import AddPostCard from "../components/AddPostCard";
 
 jest.mock("../components/AddPostModal", () => {
